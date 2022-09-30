817c897448a98a8f312a42139925b57d9104a4a2
mariavickk <111583020+mariavickk@users.noreply.github.com>
2022-09-30 12:00:06 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -1,1 +1,21 @@
+let nome = "Maria";
+let número = 35; 
+let turma = "D";
+let série = "1";
 
+function escreva(){
+    document.write()
+}
+let tabuada = 2;
+function escreva (){
+    document.write(tabuada + " X 1 = "+(tabuada*1)+"<br>");
+    document.write(tabuada + " X 2 = "+(tabuada*2)+"<br>");
+    document.write(tabuada + " X 3 = "+(tabuada*3)+"<br>");
+    document.write(tabuada + " X 4 = "+(tabuada*4)+"<br>");
+    document.write(tabuada + " X 5 = "+(tabuada*5)+"<br>");
+    document.write(tabuada + " X 6 = "+(tabuada*6)+"<br>");
+    document.write(tabuada + " X 7 = "+(tabuada*7)+"<br>");
+    document.write(tabuada + " X 8 = "+(tabuada*8)+"<br>");
+    document.write(tabuada + " X 9 = "+(tabuada*9)+"<br>");
+    document.write(tabuada + " X 10 = "+(tabuada*10)+"<br>");
+}
