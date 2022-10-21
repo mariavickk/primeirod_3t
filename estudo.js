3eb32e63a0766c0a445924f49f66d42a42359aa9
mariavickk <111583020+mariavickk@users.noreply.github.com>
2022-10-21 11:40:53 -0300

    Add files via upload

--- estudo.js
+++ estudo.js
@@ -1,21 +1,48 @@
-let nome = "Maria";
-let número = 35; 
-let turma = "D";
-let série = "1";
-
-function escreva(){
-    document.write()
+function total(){
+    let c = document.getElementById("valor").value;
+    let j = document.getElementById("juros").value;
+    let t = document.getElementById("meses").value;
+    let r = 0;
+    for(let i = 1; i <= t; i++){
+        r = c * (1 + (j/100));
+        document.write("Mês " + i + " valor: " + r +"<br>");
+        c = r;
+    }
+    
+    document.write("Resultado: " + r);
 }
-let tabuada = 2;
-function escreva (){
-    document.write(tabuada + " X 1 = "+(tabuada*1)+"<br>");
-    document.write(tabuada + " X 2 = "+(tabuada*2)+"<br>");
-    document.write(tabuada + " X 3 = "+(tabuada*3)+"<br>");
-    document.write(tabuada + " X 4 = "+(tabuada*4)+"<br>");
-    document.write(tabuada + " X 5 = "+(tabuada*5)+"<br>");
-    document.write(tabuada + " X 6 = "+(tabuada*6)+"<br>");
-    document.write(tabuada + " X 7 = "+(tabuada*7)+"<br>");
-    document.write(tabuada + " X 8 = "+(tabuada*8)+"<br>");
-    document.write(tabuada + " X 9 = "+(tabuada*9)+"<br>");
-    document.write(tabuada + " X 10 = "+(tabuada*10)+"<br>");
-}
+function adicao(){
+    let val1 = document.getElementById("v1").value;
+    let val2 = document.getElementById("v2").value;
+    let r = Number(val1) + Number(val2);
+    document.getElementById("resultado").innerHTML =r;
+    }
+    
+    function subtracao(){
+      let val1 = document.getElementById("v1").value;
+      let val2 = document.getElementById("v2").value;
+      let r = Number(val1) - Number(val2);
+      document.getElementById("resultado").innerHTML =r;
+      }
+    
+      function divisao(){
+        let val1 = document.getElementById("v1").value;
+        let val2 = document.getElementById("v2").value;
+        let r = Number(val1) / Number(val2);
+        document.getElementById("resultado").innerHTML =r;
+        }
+      
+        function multiplicacao(){
+          let val1 = document.getElementById("v1").value;
+          let val2 = document.getElementById("v2").value;
+          let r = Number(val1) * Number(val2);
+          document.getElementById("resultado").innerHTML =r;
+          }
+          function porcentagem(){
+            let val1 = document.getElementById("v1").value;
+            let val2 = document.getElementById("v2").value;
+            let c = 100
+            let p = Number(val2)  / Number(c)
+            let r = Number(val1) * Number(p);
+            document.getElementById("resultado").innerHTML =r;
+            }
