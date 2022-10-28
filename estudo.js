b2da1be870ef13ed11c1a96054db6db8a716e350
mariavickk <111583020+mariavickk@users.noreply.github.com>
2022-10-28 11:25:22 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -1,15 +1,41 @@
+function moeda(atual) {
+     return atual.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
+      
+  }
+
 function total(){
     let c = document.getElementById("valor").value;
     let j = document.getElementById("juros").value;
     let t = document.getElementById("meses").value;
-    let r = 0;
+    if (!Number(c)) {
+       alert("O Capital deve ser números.");
+       document.getElementById("valor").value = "";
+       document.getElementById("valor").focus();
+       return 
+    }
+    if (!Number(j)) {
+      alert("O Juros deve ser números.");
+      document.getElementById("juros").value = "";
+      document.getElementById("juros").focus();
+      return 
+   }
+   if (!Number(t)) {
+    alert("O Meses deve ser números.");
+    document.getElementById("meses").value = "";
+    document.getElementById("meses").focus();
+    return 
+ }
+    let mes = "";
     for(let i = 1; i <= t; i++){
         r = c * (1 + (j/100));
-        document.write("Mês " + i + " valor: " + r +"<br>");
+        mes += "Mês "+ i + " valor: " + moeda(r) +"<br>";
+        //document.write("Mês " + i + " valor: " + moeda(r) +"<br>");
         c = r;
     }
+    document.getElementById("mes").innerHTML=mes;
     
-    document.write("Resultado: " + r);
+    document.getElementById("total").innerHTML="Total: "+moeda(r);
+    //document.write("Resultado: " + moeda(r));
 }
 function adicao(){
     let val1 = document.getElementById("v1").value;
